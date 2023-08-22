e064a813e73c316f3fe7f581e16925be440affa4
Nigini Oliveira <nigini@gmail.com>
2023-08-22 15:57:59 -0700

    Added new results page with summary of task agreement between participant and AI.

--- src/litw-values/study-main.js
+++ src/litw-values/study-main.js
@@ -215,6 +215,49 @@ module.exports = (function(exports) {
 	}
 
 	function showResults() {
+		if(Object.keys(params.task_answers).length==0){
+			//TEST DATA
+			params.task_answers = JSON.parse('{' +
+				'"T0_A1":{"answer":[1,2],"correct":true,"time":4204},"T0_AI":{"answer":[1,2],"correct":true,"time":4204},"T0_A2":{"answer":[1,2],"correct":true,"time":4679},' +
+				'"T2_A1":{"answer":[1,2],"correct":false,"time":13019},"T2_AI":{"answer":[1,4],"correct":true,"time":13019},"T2_A2":{"answer":[1,4],"correct":true,"time":38018},' +
+				'"T10_A1":{"answer":[1,2],"correct":false,"time":17242},"T10_AI":{"answer":[2,4],"correct":false,"time":17242},"T10_A2":{"answer":[1,4],"correct":true,"time":9198},' +
+				'"T14_A1":{"answer":[1,2],"correct":false,"time":14307},"T14_AI":{"answer":[1,4],"correct":true,"time":14308},"T14_A2":{"answer":[1,4],"correct":true,"time":9472},' +
+				'"T1_A1":{"answer":[1,2],"correct":false,"time":25247},"T1_AI":{"answer":[1,2],"correct":false,"time":25247},"T1_A2":{"answer":[1,2],"correct":false,"time":13361},' +
+				'"T15_A1":{"answer":[1,2],"correct":false,"time":21005},"T15_AI":{"answer":[2,4],"correct":true,"time":21005},"T15_A2":{"answer":[2,4],"correct":true,"time":14367}' +
+			'}');
+		}
+
+		let user_responses = Object.keys(params.task_answers).filter(function(elem){return elem.includes('A1')});
+		let results_data = {
+			correct_a1: 0,
+			correct_ai: 0,
+			agreed: 0,
+			ai_helped: 0,
+			ai_wronged: 0
+		}
+		for(let r of user_responses) {
+			let a1 = params.task_answers[r];
+			let a2 = params.task_answers[r.replace('A1','A2')];
+			let ai = params.task_answers[r.replace('A1','AI')];
+			if(a1.correct) results_data.correct_a1++;
+			if(ai.correct) results_data.correct_ai++;
+			if(a2.correct == ai.correct) results_data.agreed++;
+			if(a1.correct && !ai.correct && !a2.correct) results_data.ai_wronged++;
+			if(!a1.correct && ai.correct && a2.correct) results_data.ai_helped++;
+		}
+		let r_data = {
+			results: results_data
+		}
+		if('PID' in params.URL) {
+			r_data.code = LITW.data.getParticipantId();
+		}
+		$("#results").html(resultsTemplate(r_data));
+		addResultsFooter();
+		$("#results").i18n();
+		LITW.utils.showSlide("results");
+	}
+
+	function showResultsValueMap() {
 		if(!params.values_data){
 			//TEST DATA
 			params.values_data = {q1:"1",q2:"2",q3:"3",q4:"4",q5:"3",q6:"2",q7:"1",q8:["uns","obd"],q9:"1",q10:"2",q11:"3"};
@@ -227,6 +270,12 @@ module.exports = (function(exports) {
 			resultsData.code = LITW.data.getParticipantId();
 		}
 		$("#results").html(resultsTemplate(resultsData));
+		addResultsFooter();
+		$("#results").i18n();
+		LITW.utils.showSlide("results");
+	}
+
+	function addResultsFooter(){
 		$("#results-footer").html(resultsFooter(
 			{
 				share_url: "https://labinthewild.org",
@@ -246,8 +295,6 @@ module.exports = (function(exports) {
 				}]
 			}
 		));
-		$("#results").i18n();
-		LITW.utils.showSlide("results");
 	}
 	function readSummaryData() {
 		$.getJSON( "summary.json", function( data ) {
