4b0a410b657adb57b29e2a54a7b9273b98f72e8b
Nigini Oliveira <nigini@gmail.com>
2023-11-07 18:09:07 -0800

    Improved UX and messaging in AI conversation.

--- src/litw-values/study-main.js
+++ src/litw-values/study-main.js
@@ -167,10 +167,10 @@ module.exports = (function(exports) {
 		// timeline.push(params.slides.IRB);
 		// timeline.push(params.slides.DEMOGRAPHICS);
 		// timeline.push(params.slides.VALUES_Q);
-		// timeline.push(params.slides.AI_CONVO);
+		timeline.push(params.slides.AI_CONVO);
 
 		// TASK added alone here for testing!
-		timeline.push(params.slides.TASK);
+		// timeline.push(params.slides.TASK);
 		// timeline.push(params.slides.AI_IMPRESSIONS);
 
 		// if(params.ai_impressions_before_task) {
